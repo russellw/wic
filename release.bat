0ce8c045c14c1baf11d1b5e385b354c2678802f7
Russell Wallace <russell.wallace@gmail.com>
2015-07-10 01:08:22 +0100

    release batch file

---
+++ release.bat
@@ -0,0 +1,15 @@
+set version=1
+
+call build.bat
+if errorlevel 1 goto :eof
+
+md wic-%version%
+
+copy *.exe wic-%version%
+copy *.md wic-%version%
+copy LICENSE wic-%version%
+
+del *.zip
+7z a wic-%version%.zip wic-%version% -tzip
+
+rd /q /s wic-%version%
